9227c73a154ea4a341a173bb1c553cf7c90a56ee
MnpOdox <pavilniyas@gmail.com>
2026-06-27 23:57:23 +0530

    Show CRM sale toggle in POS payment screen

--- addons/odx_craft_pos_customisation/static/src/js/partner_online_order.js
+++ addons/odx_craft_pos_customisation/static/src/js/partner_online_order.js
@@ -6,11 +6,13 @@ patch(PosOrder.prototype, {
     setup(vals) {
         super.setup(vals);
         this.online_order = Boolean(vals.online_order);
+        this.crm_sale = Boolean(vals.crm_sale);
     },
 
     serializeForORM(opts = {}) {
         const data = super.serializeForORM(opts);
         data.online_order = Boolean(this.online_order);
+        data.crm_sale = Boolean(this.crm_sale);
         return data;
     },
 });
@@ -18,5 +20,9 @@ patch(PosOrder.prototype, {
 patch(PaymentScreen.prototype, {
     toggleOnlineOrder(ev) {
         this.currentOrder.online_order = Boolean(ev.target.checked);
+    },
+
+    toggleCrmSale(ev) {
+        this.currentOrder.crm_sale = Boolean(ev.target.checked);
     },
 });
